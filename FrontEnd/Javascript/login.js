66f0348a4fb192e98bd2d5e2f8309e5b9e0a553e
kalojira <kalojira77@gmail.com>
2025-03-15 15:30:31 +0100

    token JTW

--- FrontEnd/Javascript/login.js
+++ FrontEnd/Javascript/login.js
@@ -1,4 +1,5 @@
 // Création du formulaire d'authentification dynamiquement
+
 function createLoginForm() {
     const container = document.getElementById("loginForm");
     if (!container) return;
@@ -40,9 +41,44 @@ function createLoginForm() {
     // Gestion de l'envoi du formulaire
     form.addEventListener("submit", function(event) {
         event.preventDefault();
+        authentification(emailInput.value, passwordInput.value);
         alert(`Email: ${emailInput.value}\nMot de passe: ${passwordInput.value}`);
     });
 }
 
+function authentification(emailInput, passwordInput){
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({ email: emailInput, password: passwordInput})
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Echec de l'authentification");
+            }
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem("token", data.token);
+            alert("Connexion réussie !");
+            console.log("Token reçu :", data.token);
+        })
+        .catch(error => {
+            console.error("Erreur :", error.message);
+            alert("Identifiants incorrects !");
+        });
+}
+
 // Exécuter la fonction à la fin du chargement de la page
 document.addEventListener("DOMContentLoaded", createLoginForm);
+
+/*
+1.Lire l'entrée email et password donnée par l'utilisateur, l'associer à une variable ? au format json ?.
+2.Récupérer les identifiants connus via l'api et transformer le resultat en json.
+3.Comparer les deux.
+4.Si les id sont corrects -> Donner un accès à l'utilisateur en stockant un token sur son navigateur dans le localstorage.
+5.Si les id sont incorrects (=/= 200 coté serv) -> Renvoyer une erreur et afficher un message
+ */
+
