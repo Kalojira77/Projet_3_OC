f1569a5cc726ffa7f4e6298ef398f9e945c1e23e
kalojira <kalojira77@gmail.com>
2025-03-23 22:07:36 +0100

    création des fonction d'ajout et suppression de projet. Création de fonction de gestion des modales

--- FrontEnd/Javascript/api.js
+++ FrontEnd/Javascript/api.js
@@ -1,6 +1,7 @@
 // Gestion des appels API
 
-// Fonction pour récupérer les travaux et les catégories
+
+// Fonction pour récupérer les catégories
 
 export async function getCategories() {
   try {
@@ -8,10 +9,12 @@ export async function getCategories() {
     const categories = await reponse.json();
     return categories;
   } catch (error) {
-    console.log("categories n'est pas disponible ou ne retourne rien");
+    console.error("categories n'est pas disponible ou ne retourne rien");
   }
   
 }
+
+// Fonction qui récupère le contenu de Works
 
 export async function getWorks() {
   try {
@@ -19,7 +22,58 @@ export async function getWorks() {
     const works = await reponse.json();
     return works;
   } catch (error) {
-    console.log("works n'est pas disponible ou ne retourne rien");
+    console.error("works n'est pas disponible ou ne retourne rien");
+  }
+}
+
+
+// fonction qui ajoute le nouveau projet à Works.
+
+export async function addWork(title, selectedImage, categoryId) {
+  console.log(selectedImage + "/" + title + "/" + categoryId);
+
+  const formData = new FormData();
+  formData.append("image", selectedImage); 
+  formData.append("title", title);
+  formData.append("category", categoryId);
+
+  try {
+    const response = await fetch("http://localhost:5678/api/works", {
+      method: "POST",
+      body: formData,
+      headers: {
+        "Authorization": "Bearer " + sessionStorage.getItem("token"),
+      },
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+  } catch (error) {
+    console.error("La requête a eu une erreur: " + error);
+  }
+};
+
+// Fonction qui supprime le projet selon son id dans Works
+
+export async function deleteWork(id) {
+  try {
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${sessionStorage.getItem("token")}`
+      }
+    });
+
+    if (!response.ok) {
+      console.error("Erreur lors de la suppression:", response.status);
+      return;
+    }
+
+    console.log(`Work ${id} supprimé avec succès`);
+
+  } catch (error) {
+    console.error("Erreur dans la fonction deleteWork:", error);
   }
 }
 
--- FrontEnd/Javascript/modal.js
+++ FrontEnd/Javascript/modal.js
@@ -1,29 +1,47 @@
+
 // MODAL 
-import { getWorks } from './api.js'; 
+
+// Import des fonctions depuis api.js
+
+import { getCategories, getWorks, addWork, deleteWork } from './api.js'; 
+
+// Declaration des variables globales
 
 const works = await getWorks();
 
+const modal = document.getElementById("modal");
+const modalFirst = document.getElementById("first-modal");
+const modalSecond = document.getElementById("second-modal");
+const modalTitle = document.getElementById("modal-title");
+const modalFirstBtn = document.getElementById("modal-button");
+const overlay = document.getElementById("overlay");
+const modalContent = document.getElementById("modal-content");
+
+// fonction qui ouvre la modale
 
 export async function modalOpen(){
-    const modal = document.getElementById("modal");
-    const modalTitle = document.getElementById("modal-title");
-    const modalButton = document.getElementById("modal-button");
+   
+      if (modalFirstBtn) {
+        modalFirstBtn.addEventListener("click", () => {
+          modalSwitch();
+        });
+      }
+    
+    // overlay.style.display = "block";
+    modal.style.display = "flex";
+    modalFirst.style.display ="block";
+    modalSecond.style.display ="none";
+    modalTitle.innerHTML = "<h3>Galerie Photo</h3>"
 
     modalGallery();
-
-    modal.style.display = "flex";
-    modalTitle.innerHTML = "<h3>Galerie Photo</h3>"
+    modalClose();
+    modalReturn();
   }
 
-//   function modalAddWork();
-//   // ouvre la deuxièmre modale lorsqu'on clique sur le bouton "ajout photo"
-
-//   function addWork();
-
+// Fonction qui créer la galerie dans la première modale
 
 async function modalGallery(){
-
-    const modalContent = document.getElementById("modal-content");
+    console.log("génération de la galerie")
 
     if (!works || works.length === 0) {
         modalContent.innerHTML = "<p>Aucun travail disponible.</p>";
@@ -39,42 +57,124 @@ async function modalGallery(){
         img.src = work.imageUrl;
         img.alt = work.title;
 
-        const icon = document.createElement("i");
-        icon.classList.add("fa-solid", "fa-trash-can", "delete-icon");
+        const deleteIcon = document.createElement("i");
+        deleteIcon.classList.add("fa-solid", "fa-trash-can", "delete-icon");
+        deleteIcon.setAttribute("data-id", work.id);
+
+        deleteIcon.addEventListener("click", async () => {
+          const workId = work.id;
+          console.log("Suppression de l'élément avec l'ID :", workId);
+          await deleteWork(workId);
+          updateGallery();
+        });
 
         workItem.appendChild(img);
-        workItem.appendChild(icon);
+        workItem.appendChild(deleteIcon);
         modalContent.appendChild(workItem);
     });
   }
 
-
-
-  // affiche le contenu dynamiques des travaux récupérés sur le serveur
-
-//   function deleteWork();
-//   // Supprime un élément work en fonction de son id.
+// Fonction qui ferme la modale
 
 function modalHide(){
-    modal.style.display = "none";
+  if (modal /*&& overlay*/) {
+    modal.style.display = "none";  
+    overlay.style.display = "none";  
+  }
 }
 
+// Fonction permettant de donner les conditions d'execution de la fonction de fermeture de la modale (ne fonctionne pas avec l'overlay)
+
 function modalClose(){
-const closeIcon = document.getElementById("close-icon");
-const modal = document.getElementById("modal");
+  console.log("Closing modal possibility")
+  const closeIcon1 = document.getElementById("close-icon1");
+  const closeIcon2 = document.getElementById("close-icon2");
 
-    if (closeIcon) {
-        closeIcon.addEventListener("click", () => {
-            modalHide();
-        })
+    if (closeIcon1) {
+        closeIcon1.addEventListener("click", modalHide);
+        console.log("-> icone1")
+      }
+    if (closeIcon2) {
+      closeIcon2.addEventListener("click", modalHide);
+      console.log("-> icone2")
     }
-
-    modal.addEventListener("click", (event) => {
-        if (modal.contains(event.target)) {
-            modalHide();
-        }
-    });
+    // if (overlay) {
+    //   overlay.addEventListener("click", (event) => {
+    //     console.log("overlay event");
+    //     if (event.target === overlay){
+    //       modalHide();
+    //     }
+    //   });
+    // }         
 };
 
-modalClose();
+
+// Fonction permettant le passage de la modale 1 à la modale 2
+
+function modalSwitch(){
+  modalFirst.style.display = "none";
+  modalSecond.style.display = "block";
+}
+
+async function modalGetCategory() {
+  try {
+    const category = await getCategories();
+    const categorySelector = document.getElementById("category");
+
+    if (category) {
+      category.forEach(category => {
+        const option = document.createElement("option");
+        option.value = category.id;
+        option.textContent = category.name;
+        categorySelector.appendChild(option);
+      });
+    } else {
+      console.log("Aucune catégorie disponible")
+    }
+  } catch (error) {
+    console.error("Erreur dans modalGetCategory:", error);
+  }
+}
+
+// Fonction de retour à la première modale
+
+function modalReturn(){
+  const returnBtn = document.getElementById("return");  
+
+  if (returnBtn) {
+    returnBtn.addEventListener("click", () => {
+      modalFirst.style.display = "block";   
+      modalSecond.style.display = "none";  
+    });
+  }
+}
+
+// Fonction qui récupère les informations dans le formulaire de la 2nd modale et les envoie vers addWork ./api/works
+
+function getFormContent () {
+  document.getElementById("submit-btn").addEventListener("click", async function(event) {
+    event.preventDefault();
+
+    const fileInput = document.getElementById("photo");
+    const titleInput = document.getElementById("title");
+    const categoryInput = document.getElementById("category");
+
+    if (fileInput.files.length > 0) {
+      const selectedImage = fileInput.files[0];  
+      const title = titleInput.value;
+      const categoryId = categoryInput.value;
+
+      await addWork(title, selectedImage, categoryId);
+
+    } else {
+      console.error("Aucune image sélectionnée");
+    }
+  })
+}
+
+
+modalGetCategory();
+getFormContent();
+
+
 
